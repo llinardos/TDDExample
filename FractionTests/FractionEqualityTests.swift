485454d38c53acb1dfddf75665f9cb54a074ebfb
Leandro Linardos <leandro.linardos@gmail.com>
2019-09-22 14:41:47 -0300

    Fractions supports comparing fractions.

--- FractionTests/FractionEqualityTests.swift
+++ FractionTests/FractionEqualityTests.swift
@@ -4,15 +4,30 @@ import Fraction
 class FractionEqualityTests : XCTestCase {
   // Equality test list
   func test_equals_forIntegers() {
-    XCTAssertEqual(Fraction(1), Fraction(1))
+    XCTAssertEqual(Fraction(1),   Fraction(1))
     XCTAssertEqual(Fraction(1,1), Fraction(1))
-    XCTAssertEqual(Fraction(1), Fraction(1,1))
+    XCTAssertEqual(Fraction(1),   Fraction(1,1))
     XCTAssertEqual(Fraction(1,1), Fraction(1,1))
     XCTAssertEqual(Fraction(2,1), Fraction(2,1))
     XCTAssertEqual(Fraction(2,1), Fraction(2))
+    XCTAssertEqual(Fraction(2), Fraction(2))
     XCTAssertNotEqual(Fraction(2,1), Fraction(1))
   }
-  // 1/1 == 1/1 true
+  
+  
+  func test_equals_forFractions() {
+    XCTAssertEqual(Fraction(1,3),   Fraction(1,3))
+    XCTAssertEqual(Fraction(2,3),   Fraction(2,3))
+    XCTAssertNotEqual(Fraction(2,3),   Fraction(1,3))
+    XCTAssertNotEqual(Fraction(4,3), Fraction(1,3))
+    XCTAssertEqual(Fraction(4,3), Fraction(4,3))
+    XCTAssertNotEqual(Fraction(4,3), Fraction(3,3))
+    XCTAssertEqual(Fraction(-1,3), Fraction(-1,3))
+    XCTAssertEqual(Fraction(-1,3), Fraction(1,-3))
+    XCTAssertEqual(Fraction(1,-3), Fraction(1,-3))
+    XCTAssertEqual(Fraction(1,-3), Fraction(-1,3))
+    XCTAssertEqual(Fraction(-1,-3), Fraction(-1,-3))
+  }
   // 1/3 == 1/3 true
   // 2/3 == 2/3 true
   // 2/3 == 1/3 false
